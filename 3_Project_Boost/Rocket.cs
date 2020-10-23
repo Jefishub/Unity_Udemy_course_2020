ce8ef6ee794e44dadca932623786c40c081c4f27
jefishub <yhhyhyy@hotmail.com>
2020-10-23 17:51:18 +0300

    fixed movement

--- 3_Project_Boost/Rocket.cs
+++ 3_Project_Boost/Rocket.cs
@@ -20,10 +20,11 @@ public class Rocket : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        ProcessInput();
+        Thrust();
+        Rotate();
     }
-    private void ProcessInput()
-    {
+
+    private void Thrust(){
         if (Input.GetKey(KeyCode.Space)) // can thrust while rotating
         {
             rigidBody.AddRelativeForce(Vector3.up);
@@ -31,10 +32,13 @@ public class Rocket : MonoBehaviour
                 audioSource.Play();
             }
         }
-        else{
+        else {
             audioSource.Stop();
         }
+    }
 
+    private void Rotate(){
+        rigidBody.freezeRotation = true; //take manual control of rotation
         if (Input.GetKey(KeyCode.A))
         {
             transform.Rotate(Vector3.forward);
@@ -43,5 +47,7 @@ public class Rocket : MonoBehaviour
         {
             transform.Rotate(-Vector3.forward);
         }
+
+        rigidBody.freezeRotation = false; //resume physics control of rotation
     }
 }
