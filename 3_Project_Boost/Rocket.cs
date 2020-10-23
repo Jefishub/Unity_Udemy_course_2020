b69f7d6cb514852e1ec2bad564b1ca7f1d27d2b7
jefishub <yhhyhyy@hotmail.com>
2020-10-23 17:19:59 +0300

    Audio added

--- 3_Project_Boost/Rocket.cs
+++ 3_Project_Boost/Rocket.cs
@@ -8,11 +8,13 @@ using UnityEngine;
 public class Rocket : MonoBehaviour
 {
     Rigidbody rigidBody;
+    AudioSource audioSource;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -25,6 +27,12 @@ public class Rocket : MonoBehaviour
         if (Input.GetKey(KeyCode.Space)) // can thrust while rotating
         {
             rigidBody.AddRelativeForce(Vector3.up);
+            if (audioSource.isPlaying == false){
+                audioSource.Play();
+            }
+        }
+        else{
+            audioSource.Stop();
         }
 
         if (Input.GetKey(KeyCode.A))
